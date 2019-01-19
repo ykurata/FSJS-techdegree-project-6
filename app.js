ca7558cbd0256c583b73fa283a13155c9aadc94f
ykuruta <yasukokurata@gmail.com>
2019-01-19 16:04:57 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000');
+});
